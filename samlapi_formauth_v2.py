1bfb520a54e5368a992bf4052f10fa232ce61a14
MilanNXT <milan@musec.sk>
2020-10-13 14:27:16 +0200

    v 0.0.2

--- samlapi_formauth_v2.py
+++ samlapi_formauth_v2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# https://aws.amazon.com/blogs/security/how-to-implement-federated-api-and-cli-access-using-saml-2-0-and-ad-fs/
 import sys
 # Let the script know where to find modules
 # add entry to the from of the list of paths
@@ -17,7 +18,7 @@ import os
 import configparser
 from botocore.config import Config
 
-parser = argparse.ArgumentParser(prog='samlapi_formauth', description='Obtainand store AWS credentials using ADFS Saml assertion',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(prog='samlapi_formauth2', description='Obtain AWS credentials using ADFS Saml assertion and store into shared credential file. Python 3 version.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--version', action='version', version='%(prog)s 2.0')
 parser.add_argument("-o", "--profile", dest="profile", default="saml", action="store", help="specify profile name where to store AWS credentials")
 parser.add_argument("-r", "--region", dest="region", default="eu-west-1", action="store", help="specify region where to create sts session")
@@ -33,6 +34,7 @@ parser.add_argument("-n", "--no-ssl-verify", dest="nosslverification", default=F
 parser.add_argument("-f", "--force", dest="force", default=False, action="store_true", help="force SAML token renewal")
 parser.add_argument("-e", "--set-env-vars", dest="setenvvars", default=False, action="store_true", help="store AWS credential to environmnet variables")
 parser.add_argument("-d", "--store-as-default", dest="storeasdefault", default=False, action="store_true", help="store AWS credential also as DEFAULT profile")
+parser.add_argument("-i", "--silent", dest="silent", default=False, action="store_true", help="Supress all inputs. If parameters not provided script will fail")
 parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true", help="enable verbose mode")
 args = parser.parse_args()
 
@@ -47,43 +49,54 @@ if args.verbose:
 else:
     log.basicConfig(format="%(levelname)s: %(message)s")
 
-log.info("Using following parameter values:")
-log.info(">> profile: %s", args.profile)
-log.info(">> region: %s", args.region)
-log.info(">> output: %s", args.output)
-log.info(">> role arn: %s", args.rolearn)
-log.info(">> aws config file: %s", args.awsconfigfile)
-log.info(">> idp entry url: %s", args.idpentryurl)
-log.info(">> username: %s", args.username)
-log.info(">> session duration: %s", args.sessionduration)
-log.info(">> proxy: %s", args.proxy)
-log.info(">> no ssl verification: %s", args.nosslverification)
-log.info(">> force: %s", args.force)
-log.info(">> set env vars: %s", args.setenvvars)
-log.info(">> store as default: %s", args.storeasdefault)
+log.info("Using following input parameter values:")
+log.info(">> profile: [%s]", args.profile)
+log.info(">> region: [%s]", args.region)
+log.info(">> output: [%s]", args.output)
+log.info(">> role arn: [%s]", args.rolearn)
+log.info(">> aws config file: [%s]", args.awsconfigfile)
+log.info(">> idp entry url: [%s]", args.idpentryurl)
+log.info(">> username: [%s]", args.username)
+log.info(">> session duration: [%s]", args.sessionduration)
+log.info(">> proxy: [%s]", args.proxy)
+log.info(">> no ssl verification: [%s]", args.nosslverification)
+log.info(">> force: [%s]", args.force)
+log.info(">> set env vars: [%s]", args.setenvvars)
+log.info(">> store as default: [%s]", args.storeasdefault)
 
 ##########################################################################
 # Variables
 
-username = args.username
-password = args.password
+username = args.username.strip()
+password = args.password.strip()
+rolearn = args.rolearn.strip()
+region = args.region.strip()
+output = args.output.strip()
+profile = args.profile.strip()
 # proxy details
 proxy = {}; proxy['https'] = args.proxy
 
 ##########################################################################
 
 # Test existing credentials
-try:
-    log.info('Validating credential from profile [{0}]'.format(args.profile))
-    s3conn = boto3.Session(
-        profile_name = args.profile,
-        region_name = region
-    )
-    s3 = s3conn.resource('s3',config=Config(proxies=proxy))
-    buckets = [bucket.name for bucket in s3.buckets.all()]
-    log.info('Valid credential are still available. No need to refresh. Use --force to force resfresh')
-except:
-    log.info('Credentials expired, processing to refresh...')
+if not args.force:
+    ext = False
+    try:
+        log.info('Validating credential from profile [{0}]'.format(profile))
+        s3conn = boto3.Session(
+            profile_name = profile,
+            region_name = region
+        )
+        s3 = s3conn.resource('s3',config=Config(proxies=proxy))
+        buckets = [bucket.name for bucket in s3.buckets.all()]
+        print('Valid credential are still available. No need to refresh. Use --force to force refresh')
+        ext = True
+    except:
+        log.info('Credentials expired, processing to refresh...')
+        ext = False
+
+    if ext:
+        sys.exit(0)
 
 if 'USERDOMAIN' in os.environ:
     currentuser  =  getpass.getuser() + '@' + os.environ['USERDOMAIN']
@@ -92,19 +105,24 @@ else:
 
 # Get the federated credentials from the user
 if username in (None, '') or not username.strip():
-    print("\nInput your corp domain credentials \n")
-    username = input("Username [{0}]: ".format(currentuser))
+    print("Input your CORP domain credentials:")
+    if not args.silent:
+        username = input("Username [{0}]: ".format(currentuser))
+    else:
+        log.warning('silent mode enforced, skiping username input...')
     if username in (None, '') or not username.strip():
         username = currentuser.strip()
 
 if password in (None, '') or not password.strip():
-    password = getpass.getpass("Password for [{0}]:".format(username))
-    print(' \n')
+    if not args.silent:
+        password = getpass.getpass("Password for [{0}]:".format(username))
+    else:
+        log.warning('silent mode enforced, skiping password input...')
 
 if username in (None, '') or not username.strip():
     log.error('empty username. it must be provided...')
     quit_message()
-    quit(1)
+    sys.exit(1)
 
 log.info("Using username: [{0}]".format(username))
 
@@ -117,7 +135,6 @@ payload['password']=password
 payload['authentication']='FormsAuthentication'
 
 # Programmatically get the SAML assertion
-# Opens the IdP url and follows all of the HTTP302 redirects, and
 log.info("Calling ADFS endpoint [{0}]".format(args.idpentryurl))
 response = session.post(args.idpentryurl, data=payload, verify=not args.nosslverification)
 
@@ -135,30 +152,48 @@ del username
 del password
 
 samlresponseencoded = ''
-# Look for the SAMLResponse attribute of the input tag (determined by
-# analyzing the debug print lines above)
+authmetod = ''
+authcontext = ''
+# Look for the SAMLResponse attributes
 soup = BeautifulSoup(response.text,'html.parser')
 for inputtag in soup.find_all('input'):
     if(inputtag.get('name') == 'SAMLResponse'):
         samlresponseencoded = inputtag.get('value')
+    if(inputtag.get('name') == 'AuthMethod'):
+        authmetod = inputtag.get('value')
+    if(inputtag.get('name') == 'Context'):
+        authcontext = inputtag.get('value')
+
+# if MFA is required fail and exit
+if authmetod in (None, '') or not authmetod.strip():
+    if authmetod == 'AzureMfaServerAuthentication':
+        log.info('Authentication: {0}'.format(authmetod))
+        log.error('MFA authorization required...')
+        quit_message()
+        sys.exit(1)
 
 # Better error handling is required for production use.
 if (samlresponseencoded == ''):
     log.error('No valid ADFS assertion received, please try again...')
     log.error('Suggestion: Check your username and password or supply alternative credentials.')
     quit_message()
-    quit(1)
+    sys.exit(1)
 
 # Debug only
 # print(base64.b64decode(assertion))
 
-# # Parse the returned assertion and extract the authorized roles
+# Parse the returned assertion and extract the authorized roles
 awsroles = []
+samlsessionduration = '0'
+samlrolesessionname = 'not available'
 samlresponse = ET.fromstring(base64.b64decode(samlresponseencoded))
 for saml2attribute in samlresponse.iter('{urn:oasis:names:tc:SAML:2.0:assertion}Attribute'):
     if (saml2attribute.get('Name') == 'https://aws.amazon.com/SAML/Attributes/Role'):
         for saml2attributevalue in saml2attribute.iter('{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue'):
-            awsroles.append(saml2attributevalue.text)
+            try:
+                awsroles.append(saml2attributevalue.text.split(','))
+            except:
+                info.warning('SAML Role attribute [{0}] has incorect format, ignoring. Must be [principal_role,role_to_assume]'.format(saml2attributevalue.text))
     if (saml2attribute.get('Name') == 'https://aws.amazon.com/SAML/Attributes/SessionDuration'):
         for saml2attributevalue in saml2attribute.iter('{urn:oasis:names:tc:SAML:2.0:assertion}AttributeValue'):
             samlsessionduration = int(saml2attributevalue.text)
@@ -175,53 +210,60 @@ if len(awsroles) == 0:
     log.error("No roles to assume has been returned, please try again...")
     log.error('Suggestion: Check your username and password or supply alternative credentials.')
     quit_message()
-    quit(1)
+    sys.exit(1)
 
 # if rolearn parameter provided  try to find in saml response
 selectedroleindex = -1
-if not(args.rolearn in (None, '') or not args.rolearn.strip()):
-    log.info("Looking for role [{0}] in SAML response...".format(args.rolearn))
-    for i,j in enumerate(awsroles):
+if not(rolearn in (None, '') or not rolearn.strip()):
+    log.info("Looking for role [{0}] in SAML response...".format(rolearn))
+    for i,samlroles in enumerate(awsroles):
       selectedroleindex = i
-      if args.rolearn in j:
+      if rolearn == samlroles[1]:
         log.info("...role found and selected")
         break
-    if (selectedroleindex == len(awsroles)-1) and (args.rolearn not in awsroles[selectedroleindex]):
+    if (selectedroleindex == len(awsroles)-1) and (rolearn != awsroles[selectedroleindex][1]):
         selectedroleindex = -1
         log.info("...not found")
 
-if selectedroleindex == -1:
-    if len(awsroles) > 1:
-        i = 0
-        print("Please choose the role you would like to assume:")
-        for awsrole in awsroles:
-            print ('[', i, ']: ', awsrole.split(',')[1])
-            i += 1
-        print ("Selection: ",  end=" ")
-        selectedroleindexinput = input()
-        # Basic sanity check of input
-        if selectedroleindexinput.isdigit():
-            if int(selectedroleindexinput) in range(0, len(awsroles)):
-                selectedroleindex = int(selectedroleindexinput)
+if args.silent:
+    if selectedroleindex == -1:
+        log.error('Silent mode enforced. No Role Arn hasbeen provided nor selected')
+        quit_message()
+        sys.exit(1)
+else:
+    if selectedroleindex == -1:
+        if len(awsroles) > 1:
+            i = 0
+            print("Please choose the role you would like to assume:")
+            for samlroles in awsroles:
+                print ('[', i, ']: ', samlroles[1])
+                i += 1
+            print ("Selection: ",  end=" ")
+            selectedroleindexinput = input()
+            # Basic sanity check of input
+            if selectedroleindexinput.isdigit():
+                if int(selectedroleindexinput) in range(0, len(awsroles)):
+                    selectedroleindex = int(selectedroleindexinput)
+                else:
+                    log.error('Index out of range. Must be number between 0 nad {0}, please try again...'.format(len(awsroles)-1))
+                    quit_message()
+                    sys.exit(1)
             else:
-                log.error('Index out of range. Must be number between 0 nad {0}, please try again...'.format(len(awsroles)-1))
+                log.error('Invalid index. Must be number between 0 nad {0}, please try again...'.format(len(awsroles)-1))
                 quit_message()
-                quit(1)
+                sys.exit(1)
         else:
-            log.error('Invalid index. Must be number between 0 nad {0}, please try again...'.format(len(awsroles)-1))
-            quit_message()
-            quit(1)
-    else:
-        log.warning("only single role returned which does not match one provided as parameter") if not(args.rolearn in (None, '') or not args.rolearn.strip()) else None
-        selectedroleindex = 0
-selectedrolearn = awsroles[selectedroleindex].split(',')[1]
-selectedprincipalarn = awsroles[selectedroleindex].split(',')[0]
+            log.warning("only single role returned which does not match one provided as parameter, assuming anyway...") if not(rolearn in (None, '') or not rolearn.strip()) else None
+            selectedroleindex = 0
 
-# Use the assertion to get an AWS STS token using Assume Role with SAML
+# Select Principal Role and Role to assume
+selectedprincipalarn, selectedrolearn = awsroles[selectedroleindex]
+
+# Use returned SAML assertion to get an AWS STS token
 downgrade = False
 try:
     log.info('Assuming role [{0}]...'.format(selectedrolearn))
-    token = boto3.client('sts',config=Config(proxies=proxy)).assume_role_with_saml(
+    token = boto3.client('sts',config=Config(proxies=proxy,region_name=region)).assume_role_with_saml(
         RoleArn=selectedrolearn,
         PrincipalArn=selectedprincipalarn,
         SAMLAssertion=samlresponseencoded,
@@ -233,15 +275,14 @@ except:
     log.warning("Could not assume role [{0}]".format(selectedrolearn))
     for x in range(len(e)):
         log.warning(">>> {0}".format(e[x]))
-    log.warning('Will try to downgrade session duration...')
     downgrade = True
 
 # if previous call failed then try to call again with downgraded session duration
 if downgrade:
-    log.info('Downgrading session duration to 28800 seconds...')
+    log.warning('Trying again with session duration downgrade to 28800 seconds...')
     try:
         log.info('Assuming role [{0}]...'.format(selectedrolearn))
-        token = boto3.client('sts',config=Config(proxies=proxy)).assume_role_with_saml(
+        token = boto3.client('sts',config=Config(proxies=proxy,region_name=region)).assume_role_with_saml(
             RoleArn=selectedrolearn,
             PrincipalArn=selectedprincipalarn,
             SAMLAssertion=samlresponseencoded,
@@ -255,7 +296,7 @@ if downgrade:
             log.error(">>> {0}".format(e[x]))
         log.error('Suggestion: Check your username and password or supply alternative credentials.')
         quit_message()
-        quit(1)
+        sys.exit(1)
 
 # Write the AWS STS token into the AWS credential file
 home = os.path.expanduser("~")
@@ -266,20 +307,20 @@ config = configparser.RawConfigParser()
 config.read(filename)
 
 # Put the credentials into saml profile
-if not config.has_section(args.profile):
-    config.add_section(args.profile)
-config.set(args.profile, 'output', args.output)
-config.set(args.profile, 'region', args.region)
-config.set(args.profile, 'aws_access_key_id', token['Credentials']['AccessKeyId'])
-config.set(args.profile, 'aws_secret_access_key', token['Credentials']['SecretAccessKey'])
-config.set(args.profile, 'aws_session_token', token['Credentials']['SessionToken'])
+if not config.has_section(profile):
+    config.add_section(profile)
+config.set(profile, 'output', output)
+config.set(profile, 'region', region)
+config.set(profile, 'aws_access_key_id', token['Credentials']['AccessKeyId'])
+config.set(profile, 'aws_secret_access_key', token['Credentials']['SecretAccessKey'])
+config.set(profile, 'aws_session_token', token['Credentials']['SessionToken'])
 
 # Put the credentials into default profile
 if args.storeasdefault:
     if not config.has_section('default'):
         config.add_section('default')
-    config.set('default', 'output', args.output)
-    config.set('default', 'region', args.region)
+    config.set('default', 'output', output)
+    config.set('default', 'region', region)
     config.set('default', 'aws_access_key_id', token['Credentials']['AccessKeyId'])
     config.set('default', 'aws_secret_access_key', token['Credentials']['SecretAccessKey'])
     config.set('default', 'aws_session_token', token['Credentials']['SessionToken'])
@@ -296,15 +337,17 @@ if args.setenvvars:
     os.environ['AWS_SESSION_TOKEN']=token['Credentials']['SessionToken']
 else:
     log.info('Removing AWS credentials from environment variables')
-    del os.environ['AWS_ACCESS_KEY_ID']
-    del os.environ['AWS_SECRET_ACCESS_KEY']
-    del os.environ['AWS_SESSION_TOKEN']
-
+    if 'AWS_ACCESS_KEY_ID' in os.environ:
+        del os.environ['AWS_ACCESS_KEY_ID']
+    if 'AWS_SECRET_ACCESS_KEY' in os.environ:
+        del os.environ['AWS_SECRET_ACCESS_KEY']
+    if 'AWS_SESSION_TOKEN' in os.environ:
+        del os.environ['AWS_SESSION_TOKEN']
 # Give the user some basic info as to what has just happened
 log.info('----------------------------------------------------------------')
-log.info('Your new access key pair has been stored in the AWS configuration file {0} under the [{1}] profile.'.format(filename, args.profile))
+log.info('Your new access key pair has been stored in the AWS configuration file {0} under the [{1}] profile.'.format(filename, profile))
 log.info('Note that it will expire at {0}.'.format(token['Credentials']['Expiration']))
 log.info('After this time, you may safely rerun this script to refresh your access key pair.')
-log.info('To use this credential, call the AWS CLI with the --profile option (e.g. aws --profile {0} ec2 describe-instances).'.format(args.profile))
+log.info('To use this credential, call the AWS CLI with the --profile option (e.g. aws --profile {0} ec2 describe-instances).'.format(profile))
 log.info('----------------------------------------------------------------')
 print('...success')
